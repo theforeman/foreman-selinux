8bf6a80cd275792ae65ab700d95701dbb3f7cdb8
Evgeni Golov <evgeni@golov.de>
2024-10-30 15:51:51 +0100

    drop loop around selinux variants, we only support targeted

--- common/selinux-disable.sh
+++ common/selinux-disable.sh
@@ -4,11 +4,9 @@ cat <<EOF
 #!/bin/bash
 set +e
 
-for selinuxvariant in targeted
-do
-  if /usr/sbin/semodule -s \$selinuxvariant -l >/dev/null; then
-    # Unload policy
-    /usr/sbin/semodule -s \$selinuxvariant -r $MODULE
-  fi
-done
+selinuxvariant=targeted
+if /usr/sbin/semodule -s \$selinuxvariant -l >/dev/null; then
+  # Unload policy
+  /usr/sbin/semodule -s \$selinuxvariant -r $MODULE
+fi
 EOF
--- common/selinux-enable.sh
+++ common/selinux-enable.sh
@@ -4,12 +4,10 @@ cat <<EOF
 #!/bin/bash
 set +e
 
-for selinuxvariant in targeted
-do
-  if /usr/sbin/semodule -s \$selinuxvariant -l >/dev/null; then
-    # Load policy
-    /usr/sbin/semanage module -S \$selinuxvariant \
-      -a /usr/share/selinux/\${selinuxvariant}/${MODULE}.pp.bz2
-  fi
-done
+selinuxvariant=targeted
+if /usr/sbin/semodule -s \$selinuxvariant -l >/dev/null; then
+  # Load policy
+  /usr/sbin/semanage module -S \$selinuxvariant \
+    -a /usr/share/selinux/\${selinuxvariant}/${MODULE}.pp.bz2
+fi
 EOF
